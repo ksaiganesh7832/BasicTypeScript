98465330fd3af26f9dad26455b295b4c1ba94f2e
ksaiganesh7832 <ksaiganesh7832@gmail.com>
2026-06-12 09:32:18 +0530

    New 12th june

---
+++ FunctionPrograms/New.ts
@@ -0,0 +1,11 @@
+function calculating() : string // Number is a retunr type with number value and without parameters
+{
+    let a : number = 32;
+    let b : number = 31;
+    let p : string = "Ganesh"
+    
+    return a+p+b;
+    
+}
+
+console.log(calculating());    // First--->We are calling the function with the function name Secomd--->We are gettimg the return value
